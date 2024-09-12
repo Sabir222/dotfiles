2b1f49d8b55f0865184f7c71131574987aea6961
sabir222 <mrsabir4@gmail.com>
2024-09-12 17:15:46 +0100

    update: updating nvim  cfg

--- .config/nvim/lua/custom/plugins/treesitter-textobject.lua
+++ .config/nvim/lua/custom/plugins/treesitter-textobject.lua
@@ -27,36 +27,42 @@ return {
             ['at'] = '@comment.outer',
           },
         },
-        -- move = {
-        --   enable = true,
-        --   set_jumps = true, -- whether to set jumps in the jumplist
-        --   goto_next_start = {
-        --     [']f'] = '@function.outer',
-        --     [']]'] = '@class.outer',
-        --   },
-        --   goto_next_end = {
-        --     [']F'] = '@function.outer',
-        --     [']['] = '@class.outer',
-        --   },
-        --   goto_previous_start = {
-        --     ['[f'] = '@function.outer',
-        --     ['[['] = '@class.outer',
-        --   },
-        --   goto_previous_end = {
-        --     ['[F'] = '@function.outer',
-        --     ['[]'] = '@class.outer',
-        --   },
-        -- },
-        -- swap = {
-        --   enable = true,
-        --   swap_next = {
-        --     ['<leader>a'] = '@parameter.inner',
-        --   },
-        --   swap_previous = {
-        --     ['<leader>A'] = '@parameter.inner',
-        --   },
-        -- },
+        move = {
+          enable = true,
+          set_jumps = true, -- whether to set jumps in the jumplist
+          goto_next_start = {
+            [']f'] = '@function.outer',
+            [']]'] = '@class.outer',
+          },
+          goto_next_end = {
+            [']F'] = '@function.outer',
+            [']['] = '@class.outer',
+          },
+          goto_previous_start = {
+            ['[f'] = '@function.outer',
+            ['[['] = '@class.outer',
+          },
+          goto_previous_end = {
+            ['[F'] = '@function.outer',
+            ['[]'] = '@class.outer',
+          },
+        },
+        swap = {
+          enable = false,
+          swap_next = {
+            ['<leader>a'] = '@parameter.inner',
+          },
+          swap_previous = {
+            ['<leader>A'] = '@parameter.inner',
+          },
+        },
       },
     }
+
+    local ts_repeat_move = require 'nvim-treesitter.textobjects.repeatable_move'
+
+    -- vim way: ; goes to the direction you were moving.
+    vim.keymap.set({ 'n', 'x', 'o' }, ';', ts_repeat_move.repeat_last_move)
+    vim.keymap.set({ 'n', 'x', 'o' }, ',', ts_repeat_move.repeat_last_move_opposite)
   end,
 }
