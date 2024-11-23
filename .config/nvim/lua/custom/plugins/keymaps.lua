e8f105c01b4c10e5babad01f2a042c706254796d
sabir222 <mrsabir4@gmail.com>
2024-11-23 02:07:05 +0100

    update:cfg

--- .config/nvim/lua/custom/plugins/keymaps.lua
+++ .config/nvim/lua/custom/plugins/keymaps.lua
@@ -25,4 +25,6 @@ return {
   vim.keymap.set({ 'n', 'x', 'o' }, 'f', '<Plug>(leap-forward)'),
   vim.keymap.set({ 'n', 'x', 'o' }, 'F', '<Plug>(leap-backward)'),
   vim.keymap.set({ 'n', 'x', 'o' }, 'gf', '<Plug>(leap-from-window)'),
+  -- toggle terminal
+  vim.keymap.set({ 'n', 'v' }, '<C-`>', ':ToggleTerm<CR>', {}),
 }
---
+++ .config/nvim/lua/custom/plugins/toggle-terminal.lua
@@ -0,0 +1,3 @@
+return {
+  { 'akinsho/toggleterm.nvim', lazy = true, cmd = 'ToggleTerm', version = '*', config = true },
+}
