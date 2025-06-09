bcc38dfc5d91558895ffe66d8bca4c48938ede87
sabir222 <mrsabir4@gmail.com>
2025-06-09 03:03:02 +0100

    update: ✨ cfg

--- nixos/modules/packages.nix
+++ nixos/modules/packages.nix
@@ -1,5 +1,20 @@
 { pkgs, ... }: {
-  environment.systemPackages = with pkgs; [ ];
+  environment.systemPackages = with pkgs; [
+    bibata-cursors
+  ];
+
+  # Set cursor theme system-wide
+  environment.variables = {
+    XCURSOR_THEME = "Bibata-Modern-Ice";
+    XCURSOR_SIZE = "24";
+  };
+
+  # Optional: helps set cursor theme for X apps (even under Wayland)
+  services.xserver = {
+    exportConfiguration = true;
+  };
+
   programs.firefox.enable = true;
+
   nixpkgs.config.allowUnfree = true;
 }
