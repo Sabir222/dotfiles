2156f9e1b9ce152427a22c391f28d2a31158a723
sabir222 <mrsabir4@gmail.com>
2025-06-09 14:17:06 +0100

    update: ✨ cfg

--- .config/nvim/lua/sabir/plugins/kickstart-plugins/auto-format.lua
+++ .config/nvim/lua/sabir/plugins/kickstart-plugins/auto-format.lua
@@ -32,6 +32,7 @@ return { -- Autoformat
     end,
     formatters_by_ft = {
       lua = { 'stylua' },
+      nix = { 'nixpkgs_fmt' },
       -- Conform can also run multiple formatters sequentially
       python = { 'black' },
       -- You can use 'stop_after_first' to run the first available formatter from the list
