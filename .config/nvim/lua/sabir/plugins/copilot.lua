e1d617590961281a563555fa70afc63717a53766
sabir222 <mrsabir4@gmail.com>
2025-07-30 22:36:41 +0100

    update: ✨ cfg

--- .config/nvim/lua/sabir/plugins/copilot.lua
+++ .config/nvim/lua/sabir/plugins/copilot.lua
@@ -1,4 +1,42 @@
 return {
+  {
+    'zbirenbaum/copilot.lua',
+    event = 'VeryLazy',
+    config = function()
+      require('copilot').setup {
+        suggestion = {
+          enabled = true,
+          auto_trigger = false,
+          accept = false,
+        },
+        panel = {
+          enabled = false,
+        },
+        filetypes = {
+          markdown = true,
+          help = true,
+          html = true,
+          javascript = true,
+          typescript = true,
+          ['*'] = true,
+        },
+      }
+
+      vim.keymap.set('i', '<Tab>', function()
+        if require('copilot.suggestion').is_visible() then
+          require('copilot.suggestion').accept()
+        else
+          vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Tab>', true, false, true), 'n', false)
+        end
+      end, {
+        silent = true,
+      })
+      --toggle_auto_trigger
+      vim.keymap.set('n', '<leader>tc', function()
+        require('copilot.suggestion').toggle_auto_trigger()
+      end, { silent = true, desc = 'Toggle Copilot auto-trigger' })
+    end,
+  },
   -- {
   --   'zbirenbaum/copilot.lua',
   --   enabled = true,
