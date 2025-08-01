caa6c5395e10c075b66b13a6476afba3212a0b3c
sabir222 <mrsabir4@gmail.com>
2025-08-01 20:23:20 +0100

    update: ✨ cfg

--- .config/nvim/lua/sabir/plugins/lsp-lines.lua
+++ .config/nvim/lua/sabir/plugins/lsp-lines.lua
@@ -1,19 +1,19 @@
 return {
-  lazy = true,
-  'https://git.sr.ht/~whynothugo/lsp_lines.nvim',
-  config = function()
-    require('lsp_lines').setup {
-      -- Your optional custom configuration can go here
-    }
-
-    -- Toggle off lsp_lines on startup
-    vim.api.nvim_create_autocmd('VimEnter', {
-      callback = function()
-        require('lsp_lines').toggle()
-      end,
-    })
-
-    -- Keymap to toggle lsp_lines
-    vim.keymap.set('n', '<Leader>e', require('lsp_lines').toggle, { desc = 'Toggle lsp_lines' })
-  end,
+  -- lazy = true,
+  -- 'https://git.sr.ht/~whynothugo/lsp_lines.nvim',
+  -- config = function()
+  --   require('lsp_lines').setup {
+  --     -- Your optional custom configuration can go here
+  --   }
+  --
+  --   -- Toggle off lsp_lines on startup
+  --   vim.api.nvim_create_autocmd('VimEnter', {
+  --     callback = function()
+  --       require('lsp_lines').toggle()
+  --     end,
+  --   })
+  --
+  --   -- Keymap to toggle lsp_lines
+  --   vim.keymap.set('n', '<Leader>e', require('lsp_lines').toggle, { desc = 'Toggle lsp_lines' })
+  -- end,
 }
