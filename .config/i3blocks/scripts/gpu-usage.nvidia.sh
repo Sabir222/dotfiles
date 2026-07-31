ead9a05ef821481ba5e81e2c81d0149faa9fa7ad
sabir222 <mrsabir4@gmail.com>
2026-07-31 23:05:31 +0100

    update: ✨ cfg

--- .config/i3blocks/scripts/gpu-usage.nvidia.sh
+++ .config/i3blocks/scripts/gpu-usage.nvidia.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+if ! command -v nvidia-smi &>/dev/null; then
+    echo "n/a"
+    exit 0
+fi
 nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits | awk '{print $1 "%"}'
---
+++ .config/waybar/swaybar-cmd.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+exec waybar -c /home/sabir/.config/waybar/config-sway.jsonc -s /home/sabir/.config/waybar/style-sway.css "$@"
